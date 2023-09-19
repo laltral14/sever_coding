a1ed3891677f899affed6d20c9a6d6589d3dafa0
laltral14 <79060032+laltral14@users.noreply.github.com>
2023-09-19 21:48:13 +0300

    Update recommendation.py

--- services/recommendation.py
+++ services/recommendation.py
@@ -4,7 +4,8 @@ import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.preprocessing import LabelEncoder
 import pandas as pd
-
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
 
 def get_prices(search, qdrant):
     search_res = qdrant.similarity_search_with_score(search, k=100, score_threshold=0.62, offset=1)
@@ -36,25 +37,31 @@ def prepare_data(data):
     return user_item_matrix, le_user, le_item, user_similarity
 
 
-def get_item_recommendations(data, user_id, num_recommendations=5):
-    user_item_matrix, le_user, le_item, user_similarity = prepare_data(data)
-
-    user_idx = le_user.transform([user_id])[0]
-
-    # Находим средний рейтинг пользователя
-    user_ratings = user_item_matrix.iloc[user_idx]
-    user_mean_rating = user_ratings.mean()
-
-    # Считаем взвешенное сходство между пользователями и их рейтингами
-    weighted_similarities = user_similarity[user_idx] * (user_ratings - user_mean_rating)
-
-    # Получаем индексы товаров, которые пользователь еще не покупал
-    unrated_items_idx = user_ratings[user_ratings == 0].index
-
-    # Сортируем товары по весу сходства и выбираем наиболее релевантные
-    recommendations = unrated_items_idx[np.argsort(weighted_similarities[unrated_items_idx])[-num_recommendations:]]
-
-    # Переводим индексы товаров обратно в их исходные значения
-    recommended_items = le_item.inverse_transform(recommendations)
-
+def get_item_recommendations(rec_data, item_id):
+    
+    # Преобразование данных в бинарный формат (транзакции в столбцы с категориями)
+    df_encoded = pd.get_dummies(rec_data['список покупок'].apply(pd.Series).stack()).sum(level=0)
+    df_encoded = df_encoded.astype(bool).astype(int)
+    
+    # Задайте ID товара, для которого нужно получить рекомендации
+    target_item = item_id
+    
+    # Подготовьте данные только для транзакций, в которых есть целевой товар
+    df_target = df_encoded[df_encoded[target_item] == 1]
+    
+    # Применение алгоритма Apriori для поиска частых наборов
+    frequent_itemsets = apriori(df_target, min_support=0.1, use_colnames=True)
+    
+    # Поиск ассоциативных правил
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=0.5)
+    
+    # Фильтрация правил для исключения target_item из консеквентов
+    filtered_rules = rules[~rules['consequents'].apply(lambda x: target_item in x)]
+    
+    # Отсортируйте правила по убыванию по показателю "lift"
+    sorted_rules = filtered_rules.sort_values(by='lift', ascending=False)
+    
+    # Выведите рекомендации для заданного товара без target_item
+    recommended_items = list(sorted_rules['consequents'].iloc[0])
+    
     return recommended_items
